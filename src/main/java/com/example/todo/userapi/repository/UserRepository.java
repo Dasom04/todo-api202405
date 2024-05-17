687a2efab7e831810dcde49b28bc7daec308cab7
Dasom04 <slz_952@naver.com>
2024-05-17 17:43:24 +0900

    #3. user api 기본 구성, 이메일 중복 확인 요청 처리

---
+++ src/main/java/com/example/todo/userapi/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.todo.userapi.repository;
+
+import com.example.todo.userapi.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface UserRepository extends JpaRepository<User, String> {
+
+    // 이메일 중복 체크
+//    @Query("SELECT COUNT(*) FROM User u WHERE u.email =: email") -> JPQL
+    boolean existsByEmail(String email);
+
+}
