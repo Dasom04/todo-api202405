6ba618c18332f5b5d57b4598e9d61d0152c56902
Dasom04 <slz_952@naver.com>
2024-05-24 15:09:35 +0900

    #16 TokenProvider에 Refresh Token 발급 로직 추가

--- src/main/java/com/example/todo/userapi/dto/response/LoginResponseDTO.java
+++ src/main/java/com/example/todo/userapi/dto/response/LoginResponseDTO.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.Map;
 
 
 // 로그인 성공 후 클라이언트에게 전송할 데이터 객체
@@ -22,11 +23,11 @@ public class LoginResponseDTO {
     @JsonFormat(pattern = "yyyy년 MM월 dd일")
     private LocalDate joinDate;
 
-    private String token; // 인증 토큰 (핵심)
+    private Map<String, String> token; // 인증 토큰 (핵심)
 
     private String role; // 권한
 
-    public LoginResponseDTO(User user, String token) {
+    public LoginResponseDTO(User user, Map<String, String> token) {
         this.email = user.getEmail();
         this.userName = user.getUserName();
         this.joinDate = LocalDate.from(user.getJoinDate()); // LocalDateTime 타입이 다르면 from을 이용해서 넣어라!
